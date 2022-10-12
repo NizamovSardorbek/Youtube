5df3de692d61b8d608208e191da17d1946345260
SardorbekDevelop <sardornizamov717@gmail.com>
2022-10-12 11:08:20 +0500

    search the finish

--- src/YoutubeSearch.jsx
+++ src/YoutubeSearch.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Input, InputButton, InputWrap } from "./YoutubeStyle";
 import { FcSearch } from "react-icons/fc";
+import { Oylik } from "./TableContext";
 const YoutubeSearch = () => {
+  const [title, setTitle] = useState("");
+  const [state, setState] = useContext(Oylik);
+
+  const onSearch = () => {
+    let res = state.filter((val) =>
+      val.name.toLowerCase().includes(title.toLowerCase())
+    );
+    setState(res);
+  };
   return (
     <div>
       <InputWrap>
-        <Input type="text" placeholder="Search Movies" />
-        <InputButton>
+        <Input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Search Movies"
+        />
+        <InputButton onClick={onSearch}>
           Search
           <i>
             <FcSearch />
